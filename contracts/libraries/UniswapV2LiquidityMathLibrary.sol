04dd343a412b77ac692a735031f7434756743588
0xFlare <flame.protocol@gmail.com>
2021-02-22 13:46:21 +0100

    Uniswap -> Flameswap

--- contracts/libraries/UniswapV2LiquidityMathLibrary.sol
+++ contracts/libraries/UniswapV2LiquidityMathLibrary.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/Babylonian.sol';
-import '@uniswap/lib/contracts/libraries/FullMath.sol';
+import '@flameswap/core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@flameswap/core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@flameswap/lib/contracts/libraries/Babylonian.sol';
+import '@flameswap/lib/contracts/libraries/FullMath.sol';
 
 import './SafeMath.sol';
 import './UniswapV2Library.sol';
